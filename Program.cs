8f5c99ff1a7a4a6590e3edf955a8636c11225bab
adhi3012 <68736986+adhi3012@users.noreply.github.com>
2022-01-12 02:33:13 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+    static void Main()
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        string[] arr = new string[n];
+        for(int a=0;  a < n; a++)
+        {
+            arr[a] =Console.ReadLine();
+
+        }
+        int count = 0;
+        string input = Console.ReadLine();
+        foreach(var s in arr)
+        {
+            if (s.Contains(input))
+            {
+                count++;
+            }    
+
+        }
+        Console.WriteLine(count);
+
+
+
+
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
